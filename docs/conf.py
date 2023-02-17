405d8f2ee706ddcee396928788be2da1e53c39ef
najib errami <najib_ll@msn.com>
2023-02-17 12:32:22 +0100

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ release = '0.0.2'
 import sys, os
 
 #find module solver-----------------------------------------------------------
+sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../VRPSolverEasy"))
 
 # -- General configuration ---------------------------------------------------
