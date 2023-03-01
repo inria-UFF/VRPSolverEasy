0344ffb9cfd154ac25f37416d14958dae66d591b
Eduardo Queiroga <eduardovqueiroga@gmail.com>
2023-03-01 14:47:41 -0300

    Support to XH instance on HFVRP demo

--- VRPSolverEasy/ortools_demos/HFVRP.py
+++ VRPSolverEasy/ortools_demos/HFVRP.py
@@ -167,72 +167,162 @@ def read_hfvrp_instances(instance_name,ext_heuristic=False,time_resolution_heuri
     """Read literature instances of HFVRP by giving the name of instance
         and returns dictionary containing all elements of model"""
     instance_iter = iter(read_instance(instance_name))
-
-    nb_points = int(next(instance_iter))
-
-    next(instance_iter) # pass id depot (always 0)
-    depot_x = int(next(instance_iter))
-    depot_y = int(next(instance_iter))
-    depot_demand = int(next(instance_iter))
-    id_point = 0
+    vehicles_capacities, vehicles_fixed_costs, vehicles_var_costs = [], [], []
     data = {}
-
-    # Initialize the points with depot
-    points = [{"x": depot_x,
-               "y": depot_y,
-               "demand": depot_demand,
-               "id": id_point
-               }]
-    
-    jobs = [0]
-    for i in range(nb_points):
-        id_point += 1
-        next(instance_iter) # pass id point (take index)
-        x_coord = int(next(instance_iter))
-        y_coord = int(next(instance_iter))
-        demand = int(next(instance_iter))
-        points.append({"x": x_coord,
-                "y": y_coord,
-                "demand": demand,
-                "id": id_point})
-        jobs.append(demand)
-    
-    data["demands"] = jobs
-    
-    nb_vehicles = int(next(instance_iter))
-    vehicle_types = []
+    points = []
+    firstRead = next(instance_iter)
     index = 0
-    vehicles_capacities = []
-    vehicles_var_costs = []
-    vehicles_fixed_costs = []
-    costs_dist = {}
-    for k in range(1, nb_vehicles+1):
-        capacity = int(next(instance_iter))
-        fixed_cost = float(next(instance_iter))
-        var_cost_dist = float(next(instance_iter))
-        next(instance_iter) # pass min number
-        max_number = int(next(instance_iter))
-        vehicle_type = {"id": k,  # we cannot have an id less than 1
-                "start_point_id": 0,
-                "end_point_id": 0,
-                "capacity": capacity,
-                "max_number": max_number,
-                "fixed_cost" : fixed_cost,
-                "var_cost_dist": var_cost_dist
-                }
-        vehicle_types.append(vehicle_type)
-        for i in range(max_number):
-            vehicles_capacities.append(capacity)
-            vehicles_var_costs.append(var_cost_dist)
-            vehicles_fixed_costs.append(fixed_cost)
-            index += 1
-     
+
+    if firstRead == 'NAME': # read XH instances (large instances)
+        nb_points, nb_vehicles, max_num_veh = [], [], []
+        capacities, fixed_costs, var_costs = [], [], []
+        while True:
+            element = next(instance_iter)
+            if element == "DIMENSION":
+                next(instance_iter)  # pass ":"
+                nb_points = int(next(instance_iter)) - 1
+            elif element == "VEHICLE_KINDS":
+                next(instance_iter)  # pass ":"
+                nb_vehicles = int(next(instance_iter))
+            elif element == "CAPACITIES":
+                for k in range(nb_vehicles):
+                    capacities.append(int(next(instance_iter)))
+            elif element == "FIXED_COSTS":
+                for k in range(nb_vehicles):
+                    fixed_costs.append(int(next(instance_iter)))
+            elif element == "VARIABLE_COSTS":
+                for k in range(nb_vehicles):
+                    var_costs.append(float(next(instance_iter)))
+            elif element == "NUMBER_OF_VEHICLES":
+                for k in range(nb_vehicles):
+                    max_num_veh.append(int(next(instance_iter)))
+            elif element == "EDGE_WEIGHT_TYPE":
+                next(instance_iter)  # pass ":"
+                element = next(instance_iter)
+                if element != "EUC_2D":
+                    raise Exception("EDGE_WEIGHT_TYPE : " + element + """
+                    is not supported (only EUC_2D)""")
+            elif element == "NODE_COORD_SECTION":
+                break
+
+        vehicle_types = []
+        # Create vehicle types
+        for k in range(nb_vehicles):
+            vehicle_type = {"id": k+1,  # we cannot have an id less than 1
+                    "start_point_id": 0,
+                    "end_point_id": 0,
+                    "capacity": capacities[k],
+                    "max_number": max_num_veh[k],
+                    "fixed_cost" : fixed_costs[k],
+                    "var_cost_dist": var_costs[k]
+                    }
+            vehicle_types.append(vehicle_type)
+
+        # create copies of vehicles of the same type to run OR-Tools heuristic
+        for k in range(nb_vehicles):
+            maxNbVeh = max_num_veh[k]
+            vehicles_capacities.extend([capacities[k] for i in range(maxNbVeh)])
+            vehicles_fixed_costs.extend([fixed_costs[k] for i in range(maxNbVeh)])
+            vehicles_var_costs.extend([var_costs[k] for i in range(maxNbVeh)])
+            index += max_num_veh[k]
+
+        id_point = 0
+        # Create points
+        for current_id in range(nb_points+1):
+            point_id = int(next(instance_iter))
+            if point_id != current_id + 1:
+                raise Exception("Unexpected index")
+            x_coord = int(next(instance_iter))
+            y_coord = int(next(instance_iter))
+            points.append({"x": x_coord,
+                            "y": y_coord,
+                            "demand": -1,
+                            "id": id_point})
+            id_point += 1
+
+        element = next(instance_iter)
+        if element != "DEMAND_SECTION":
+            raise Exception("Expected line DEMAND_SECTION")
+
+        jobs = []
+        # Get the demands
+        for current_id in range(nb_points+1):
+            point_id = int(next(instance_iter))
+            if point_id != current_id + 1:
+                raise Exception("Unexpected index")
+            points[current_id]["demand"] = int(next(instance_iter))
+            jobs.append(points[current_id]["demand"])
+        data["demands"] = jobs
+
+        element = next(instance_iter)
+        if element != "DEPOT_SECTION":
+            raise Exception("Expected line DEPOT_SECTION")
+        next(instance_iter) # pass id depot
+
+        end_depot_section = int(next(instance_iter))
+        if end_depot_section != -1:
+            raise Exception("Expected only one depot.")
+
+    else: # classic instances format
+
+        nb_points = int(firstRead)
+        next(instance_iter) # pass id depot (always 0)
+        depot_x = int(next(instance_iter))
+        depot_y = int(next(instance_iter))
+        depot_demand = int(next(instance_iter))
+        id_point = 0
+
+        # Initialize the points with depot
+        points.append({"x": depot_x,
+                "y": depot_y,
+                "demand": depot_demand,
+                "id": id_point
+                })
+        
+        jobs = [0]
+        for i in range(nb_points):
+            id_point += 1
+            next(instance_iter) # pass id point (take index)
+            x_coord = int(next(instance_iter))
+            y_coord = int(next(instance_iter))
+            demand = int(next(instance_iter))
+            points.append({"x": x_coord,
+                    "y": y_coord,
+                    "demand": demand,
+                    "id": id_point})
+            jobs.append(demand) 
+        data["demands"] = jobs
+        
+        nb_vehicles = int(next(instance_iter))
+        vehicle_types = []
+        
+        for k in range(1, nb_vehicles+1):
+            capacity = int(next(instance_iter))
+            fixed_cost = float(next(instance_iter))
+            var_cost_dist = float(next(instance_iter))
+            next(instance_iter) # pass min number
+            max_number = int(next(instance_iter))
+            vehicle_type = {"id": k,  # we cannot have an id less than 1
+                    "start_point_id": 0,
+                    "end_point_id": 0,
+                    "capacity": capacity,
+                    "max_number": max_number,
+                    "fixed_cost" : fixed_cost,
+                    "var_cost_dist": var_cost_dist
+                    }
+            vehicle_types.append(vehicle_type)
+            for i in range(max_number):
+                vehicles_capacities.append(capacity)
+                vehicles_var_costs.append(var_cost_dist)
+                vehicles_fixed_costs.append(fixed_cost)
+                index += 1
+        
     data['vehicle_capacities'] = vehicles_capacities
     data['var_costs'] = vehicles_var_costs
     data['fixed_costs'] = vehicles_fixed_costs 
     data['num_vehicles'] = index
     data['depot'] = 0
-    
+
     # compute the links of graph
     links = []
     matrix = [[0 for i in range((len(points)))] for i in range(len(points))]
