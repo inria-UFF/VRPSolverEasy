0e3144667d739963d8d780cc3c57c6d36dd2bd03
Najib ERRAMI <najib_ll@msn.com>
2023-11-03 09:57:15 +0100

    version 0.1.4

--- VRPSolverEasy/src/constants.py
+++ VRPSolverEasy/src/constants.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 # VRPSolverReal informations
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 LIBRARY_WINDOWS = "bapcod-shared.dll"
 LIBRARY_LINUX = "libbapcod-shared.so"
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'VRPSolverEasy'
 copyright = '2023, ERRAMI Najib SADYKOV Ruslan UCHOA Eduardo QUEIROGA Eduardo'
 author = 'ERRAMI Najib SADYKOV Ruslan UCHOA Eduardo QUEIROGA Eduardo'
-release = '0.1.3'
+release = '0.1.4'
 
 import sys, os
 
